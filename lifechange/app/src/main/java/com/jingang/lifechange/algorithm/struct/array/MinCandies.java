dfd05d8a48ec59f76004535e9f8bc06851c1af83
李金刚 <kingkong1357@163.com>
2021-06-29 11:23:51 +0800

    【添加两个题目】

---
+++ lifechange/app/src/main/java/com/jingang/lifechange/algorithm/struct/array/MinCandies.java
@@ -0,0 +1,16 @@
+package com.jingang.lifechange.algorithm.struct.array;
+
+/**
+ * @Description:
+ * 给定一组学生的成绩，希望按照成绩给予奖励。并且给出的奖励最少。
+ * 有两条规则：
+ * 每个学生至少得到一份奖励
+ * 成绩更高的学生必须得到严格高于相邻学生的奖励数目
+ * 输入: [8, 4, 2, 1, 3, 6, 7, 9, 5]
+ * 输出: 25
+ * @Author: jingang.Li
+ * @CreateTime: 2021/6/29-9:16 AM
+ * @changeTime:
+ */
+class MinCandies {
+}
---
+++ lifechange/app/src/main/java/com/jingang/lifechange/algorithm/struct/array/SpiralMatrix.java
@@ -0,0 +1,20 @@
+package com.jingang.lifechange.algorithm.struct.array;
+
+/**
+ * @Description:
+ * 给定一个二维的矩阵，按照螺旋顺序，返回该矩阵中的元素。
+ * 输入:
+ * [[ 1, 2, 3 ],
+ * [ 4, 5, 6 ],
+ * [ 7, 8, 9 ]]
+ * 输出: [1, 2, 3, 6, 9, 8, 7, 4, 5]
+ * @Author: jingang.Li
+ * @CreateTime: 2021/6/25-9:34 AM
+ */
+class SpiralMatrix {
+    int[] getSpiralArray(int[][] twoDArray){
+
+
+        return new int[0];
+    }
+}
