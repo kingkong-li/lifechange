b392f260f1467d3c0949a1da4f25b7ad6c18e4bf
李金刚 <kingkong1357@163.com>
2021-07-03 10:10:08 +0800

    [树和计算]

---
+++ lifechange/app/src/main/java/com/jingang/lifechange/algorithm/struct/string/BaseStringOperation.java
@@ -0,0 +1,14 @@
+package com.jingang.lifechange.algorithm.struct.string;
+
+/**
+ * @Description:
+ * @Author: jingang.Li
+ * @CreateTime:2021/7/2-10:46 AM
+ * @changeTime:
+ */
+public class BaseStringOperation {
+    private String mString="abcdefg";
+    public void baseOp(){
+        mString.replace("a","b");
+    }
+}
--- lifechange/app/src/main/java/com/jingang/lifechange/algorithm/struct/tree/TreeSum.java
+++ lifechange/app/src/main/java/com/jingang/lifechange/algorithm/struct/tree/TreeSum.java
@@ -3,7 +3,7 @@ package com.jingang.lifechange.algorithm.struct.tree;
 import com.jingang.lifechange.algorithm.struct.tree.bean.TreeNode;
 
 /**
- * @Description:
+ * @Description: 给定一棵二叉树和求和，确定树上是否具有根到叶路径，使得沿路径的所有值相加等于给定的总和
  * @Author: jingang.Li
  * @CreateTime:2021/7/1-11:28 AM
  * @changeTime:
@@ -11,12 +11,10 @@ import com.jingang.lifechange.algorithm.struct.tree.bean.TreeNode;
 public class TreeSum {
     public boolean findSum(int sum, TreeNode treeNode){
         if(treeNode==null){
-            if(sum==0){
-                return true;
-            }else {
-                return false;
-            }
-
+            return false;
+        }
+        if(treeNode.left==null && treeNode.right==null){
+            return sum==treeNode.value;
         }
         sum=sum-treeNode.value;
         return findSum(sum,treeNode.left) || findSum(sum,treeNode.right);
