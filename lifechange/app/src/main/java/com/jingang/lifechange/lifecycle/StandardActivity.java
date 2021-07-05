cc85fa0b5128451c3d30b4cd0dd470967d2d9680
李金刚 <kingkong1357@163.com>
2021-07-05 10:43:59 +0800

    【serializable数据传递】

--- lifechange/app/src/main/java/com/jingang/lifechange/lifecycle/StandardActivity.java
+++ lifechange/app/src/main/java/com/jingang/lifechange/lifecycle/StandardActivity.java
@@ -6,7 +6,10 @@ import android.view.View;
 import android.widget.Button;
 
 import com.jingang.lifechange.R;
+import com.jingang.lifechange.algorithm.struct.linkedList.bean.SingleListNode;
 import com.jingang.lifechange.base.BaseActivity;
+
+import java.io.Serializable;
 
 /**
  * 启动模式 和 Activity 启动流程结合起来 讲解下
@@ -22,9 +25,20 @@ public class StandardActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent();
-                intent.setClass(StandardActivity.this, StandardActivity.class);
+                intent.setClassName("com.jingang.aidlclient",
+                        "com.jingang.aidlclient.MainActivity");
+                intent.putExtra("studentInfo",new Student(1) );
                 StandardActivity.this.startActivity(intent);
             }
         });
+    }
+
+    public static class Student implements Serializable {
+        private int mStudentNo;
+        public String Name;
+        public int sex;
+        public Student(int studentNo){
+            mStudentNo=studentNo;
+        }
     }
 }
